fbef8822721851a60bc5df1d74dd420bb928034d
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2023-02-24 14:50:56 +0100

    feat: database

--- flasktodo/__init__.py
+++ flasktodo/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 app.config['SECRET_KEY'] = '6af40c1352065273e5b63754bc18d40c'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
 from flasktodo import routes
--- flasktodo/routes.py
+++ flasktodo/routes.py
@@ -1,5 +1,12 @@
-from flask import render_template
+import os
+import secrets
+from flask import render_template, url_for, flash, redirect
 from flasktodo import app, db
+from flasktodo.forms import RegistrationForm, LoginForm
+from flasktodo.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
+
 
 todolist = [ 
     {
@@ -20,3 +27,23 @@ def home():
 @app.route("/add")
 def add_to_list():
     return render_template("add.html", title='Add')
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+
+    return render_template("register.html",title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 't3@t3.com'and form.password.data == 'test':
+            flash(f'You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Wrong!', 'danger')
+    return render_template("login.html",title="Login", form=form)
