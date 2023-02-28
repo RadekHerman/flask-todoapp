7773da19c4fd22ea9a5b73372784b61e31f062ca
RadekHerman <107422011+RadekHerman@users.noreply.github.com>
2023-02-28 15:27:41 +0100

    feat: user update

--- flasktodo/routes.py
+++ flasktodo/routes.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from flask import render_template, url_for, flash, redirect, request
 from flasktodo import app, db, bcrypt
-from flasktodo.forms import RegistrationForm, LoginForm
+from flasktodo.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flasktodo.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 import datetime
@@ -71,6 +71,21 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data        
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', form=form)
 
 
 @app.route('/calendar')
